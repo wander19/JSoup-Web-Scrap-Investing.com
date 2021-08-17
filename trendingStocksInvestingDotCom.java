811ce3aab3e1f07f8dd74489d4d124e596dd8fca
wander19 <89039034+wander19@users.noreply.github.com>
2021-08-17 17:41:22 +0530

    Add files via upload

---
+++ trendingStocksInvestingDotCom.java
@@ -0,0 +1,24 @@
+import org.jsoup.Jsoup;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.*;
+
+public class trendingStocksInvestingDotCom {
+
+	public static void main(String[] args) throws IOException {
+		
+		Document doc = Jsoup.connect("https://in.investing.com/equities/trending-stocks").get();
+		
+		for(Element row : doc.select("table.common-table.medium.js-table.js-streamable-table tr")) {
+			String name = row.select(".col-name").text();
+			String price = row.select(".u-txt-align-end.col-last").text();
+			String high = row.select(".u-txt-align-end.col-high").text();
+			String low = row.select(".u-txt-align-end.col-low").text();
+			String volume = row.select(".u-txt-align-end.col-volume").text();
+			
+			System.out.println(name + "  " + price + "  " + high + "  " + low + "  " + volume);
+		}
+	}
+}
